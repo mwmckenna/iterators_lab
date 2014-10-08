f91fae45f6f6b3d648832340d909fa8188715faa
MWMCKENNA <MWMCKENNA@GMAIL.COM>
2014-10-08 09:15:47 -0700

    initial commit

--- iterators.js
+++ iterators.js
@@ -1,5 +1,5 @@
 var iterators = {
-    max: function (numList) {
+  max: function (numList) {
       var max = -Infinity;
       for (var i = 0; i < numList.length; i++) {
         if (numList[i] > max) {
@@ -7,7 +7,64 @@ var iterators = {
         }
       }
       return max;
+    },
+
+  min: function (numList) {
+      var min = Infinity;
+      for (var i = 0; i < numList.length; i++) {
+        if (numList[i] < min) {
+          min = numList[i];
+        }
+      }
+      return min;
+    },
+
+  each: function (numList, callback) {
+    var each = [];
+    var x = 0;
+    for (var i = 0; i < numList.length; i++) {
+      callback(numList[i]);
+    } 
+  },
+
+  map:function (numList, callback) {
+  var map = [];
+  for (var i = 0; i < numList.length; i++) {
+    map.push(callback(numList[i]));
+  }
+  return(map);
+  },
+
+  filter:function (numList, callback) {
+    var newArr = [];
+    for (var i = 0; i < numList.length; i++) {
+      if (callback(numList[i]) === true) {
+        newArr.push(numList[i]);
+      }
     }
-  };
+      return newArr;
+    }, 
+
+  reduce:function (numList) {
+    var number = 0;
+
+    for (var i = 0; i < numList.length; i++) {
+      number = numList[i]+number;
+      }
+      return number;
+    },
+
+
+  reject:function (numList, callback) {
+    var newArr = [];
+    for (var i = 0; i < numList.length; i++) {
+      if (callback(numList[i]) !== true) {
+        newArr.push(numList[i]);
+      }
+    }
+      return newArr;
+    }, 
+
+};
 
 module.exports = iterators;
